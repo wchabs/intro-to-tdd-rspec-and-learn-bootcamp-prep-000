072305cef527ac8ef6f9308c07a1dc12a90c7036
William Chabla <w_chabla@hotmail.com>
2018-06-12 23:44:42 +0000

    automatically backed up by learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,1 +1,7 @@
-require_relative
+require_relative './current_age_for_birth_year.rb'
+
+puts "What year were you born?"
+
+users_age = current_age_for_birth_year(birth_year)
+
+puts "You are: " + users_age.to_s + " years old."
